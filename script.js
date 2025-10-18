e6cf92a4b48566889214ce72b049eee8412fd34b
caubengocngeck <quangle1008209@gmail.com>
2025-10-18 23:05:21 +0700

    Update script.js

--- script.js
+++ script.js
@@ -8,10 +8,10 @@ function showWish() {
 
   const wishLines = [
     `Gửi ${name} thân yêu 🌷,\n`,
-    "Hôm nay là ngày đặc biệt – ngày để tôn vinh một nửa thế giới tuyệt vời này 💖.\n\n",
+    "Sắp tới là ngày đặc biệt – ngày để tôn vinh một nửa thế giới tuyệt vời này 💖.\n\n",
     "Chúc bạn – người con gái dịu dàng, xinh đẹp và mạnh mẽ – luôn tươi cười như ánh nắng ban mai,\nluôn tự tin tỏa sáng dù ở bất cứ nơi đâu 🌞.\n\n",
     "Mong rằng cuộc sống của bạn sẽ luôn rực rỡ như những đóa hoa mùa xuân 🌸,\nngọt ngào như hương mật ong và bình yên như một bản nhạc lofi dịu nhẹ giữa đêm khuya 🌙.\n\n",
-    "Dù mai này có bận rộn, có những lúc mỏi mệt,\nhãy nhớ rằng bạn luôn xứng đáng được yêu thương,\nđược trân trọng và được hạnh phúc – không chỉ hôm nay mà là mỗi ngày trong suốt cuộc đời này 💕.\n\n",
+    "Dù mai này có bận rộn, có những lúc mỏi mệt,\nhãy nhớ rằng bạn luôn xứng đáng được yêu thương,\nđược trân trọng và được hạnh phúc – không chỉ dịp này mà là mỗi ngày trong suốt cuộc đời này 💕.\n\n",
     "Cảm ơn bạn đã mang đến thế giới này những điều đẹp đẽ,\nnhững nụ cười và sự dịu dàng hiếm có 🌼.\n\n",
     `Chúc ${name} một ngày 20/10 thật trọn vẹn,\nngập tràn yêu thương, tiếng cười và những điều kỳ diệu nhất trên thế gian này 💐💫.`
   ];
